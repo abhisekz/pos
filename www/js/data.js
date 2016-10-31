11b1f0bc9144b67fb542deb9bc6a226fc38d48cd
abhisekroy <abhisek.roy@virginmedia.co.uk>
2016-10-31 19:17:50 +0530

    first commit

---
+++ www/js/data.js
@@ -0,0 +1,48 @@
+var order = [
+    {
+        name    : 'Breakfast',
+        qty     : 3,
+        price   : '5.0'
+    },
+    {
+        name    : 'Bread',
+        qty     : 1,
+        price   : '5.0'
+    },
+    {
+        name    : 'Sausage',
+        qty     : 1,
+        price   : '10.0'
+    }
+];
+
+var foodItems = {
+    Breakfast : [
+        {
+            name    : "Pastries",
+            desc    : "croissant, pain au chocolat, pain aux raisins or almond croissant",
+            price   : "3",
+            added   : false
+        },
+        {
+            name    : "FRENCH TOAST CROISSANT",
+            desc    : "our twist on the classic - served warm and with walnut praline, crème fraiche an honey",
+            price   : "5",
+            added   : false
+        }
+    ],
+    Lunch : [
+        {
+            name    : "Pastries",
+            desc    : "croissant, pain au chocolat, pain aux raisins or almond croissant",
+            price   : "3",
+            added   : false
+        },
+        {
+            name    : "FRENCH TOAST CROISSANT",
+            desc    : "our twist on the classic - served warm and with walnut praline, crème fraiche an honey",
+            price   : "5",
+            added   : false
+        }
+    ]
+}
